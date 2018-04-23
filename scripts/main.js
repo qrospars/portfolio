ddd6c13019e69aca966ee91187f18ee6823e2de2
Rospars Quentin <rospars.quentin@outlook.com>
2018-04-23 16:21:47 +0200

    Résolution problème Include

--- scripts/main.js
+++ scripts/main.js
@@ -80,15 +80,15 @@ function addOverlay(projectName) {
             break;
         case 'Cytoo':
             document.getElementById("js-work-hover-text").innerHTML = "<div w3-include-html=\"pages/projects_sumUp/Cytoo.html\"></div>";
-            w3IncludeHTML();
+            includeHTML();
             break;
         case 'Twitch':
             document.getElementById("js-work-hover-text").innerHTML = "<div w3-include-html=\"pages/projects_sumUp/Twitch.html\"></div>";
-            w3IncludeHTML();
+            includeHTML();
             break;
         case 'Lappart':
             document.getElementById("js-work-hover-text").innerHTML = "<div w3-include-html=\"pages/projects_sumUp/Lappart.html\"></div>";
-            w3IncludeHTML();
+            includeHTML();
             break;
         default:
             alert("Erreur");
@@ -119,4 +119,33 @@ function sleep(milliseconds) {
 // Function modulo qui gerre les nombres negatifs
 function mod(n, m) {
     return ((n % m) + m) % m;
+}
+
+// Fonction Include W3
+function includeHTML() {
+    var z, i, elmnt, file, xhttp;
+    /*loop through a collection of all HTML elements:*/
+    z = document.getElementsByTagName("*");
+    for (i = 0; i < z.length; i++) {
+        elmnt = z[i];
+        /*search for elements with a certain atrribute:*/
+        file = elmnt.getAttribute("w3-include-html");
+        if (file) {
+            /*make an HTTP request using the attribute value as the file name:*/
+            xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function() {
+                if (this.readyState == 4) {
+                    if (this.status == 200) {elmnt.innerHTML = this.responseText;}
+                    if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
+                    /*remove the attribute, and call this function once more:*/
+                    elmnt.removeAttribute("w3-include-html");
+                    includeHTML();
+                }
+            }
+            xhttp.open("GET", file, true);
+            xhttp.send();
+            /*exit the function:*/
+            return;
+        }
+    }
 }
