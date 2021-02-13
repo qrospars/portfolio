6b674c47e450bb3f5216ffaa67f5554f6ab597f8
qr-desktop <rospars.quentin@outlook.com>
2021-02-13 17:59:44 +0100

    Auto stash before merge of "master" and "origin/master"

--- src/App.js
+++ src/App.js
@@ -1,8 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
-import React from 'react';
+import React, {useEffect, useRef, useState} from 'react';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Carousel from 'react-bootstrap/Carousel'
 
 function App() {
+    const [currentPageIndex, setCurrentPageIndex] = useState(0);
+    const pageRefs = [
+        useRef(null),
+        useRef(null),
+        useRef(null),
+        useRef(null),
+    ];
+    const [scrollTop, setScrollTop] = useState(0);
+    let scrolling = true;
+    useEffect(() => {
+        const onScroll = e => {
+            if (scrolling) return;
+            console.log(scrollTop);
+            scrolling = true;
+            const newScrollValue = e.target.documentElement.scrollTop;
+            if (newScrollValue > scrollTop) {
+                pageRefs[currentPageIndex + 1].current.scrollIntoView({
+                    behavior: "smooth",
+                    block: "end",
+                    inline: "nearest"
+                });
+                pageRefs[currentPageIndex + 1].current.focus({preventScroll: true});
+                setCurrentPageIndex(currentPageIndex + 1)
+            }
+            setScrollTop(e.target.documentElement.scrollTop);
+        };
+        window.addEventListener("scroll", onScroll);
+        return () => window.removeEventListener("scroll", onScroll);
+    }, [scrollTop]);
     return (
         <div className="App">
             <div className="home">
@@ -11,7 +43,7 @@ function App() {
                 </a>
             </div>
             <div id={'canvas'}/>
-            <div id="page-0" className="page">
+            <div id="page-0" className="page" ref={pageRefs[0]}>
                 <div className="menu-wrapper">
                     <div className="presentation">
                         <span className="presentation-text">
@@ -40,30 +72,52 @@ function App() {
                     </div>
                 </div>
             </div>
-            <div id="page-1" className="page page--dark">
+            <div id="page-1" className="page page--dark" ref={pageRefs[1]}>
                 <div className="work-grid">
-                    <ul id="js-work-grid" className="work-grid-list">
-                        <li id="js-work-grid-item-1" className="work-grid-list-item">
+                    <Carousel>
+                        <Carousel.Item className="work-grid-list-item">
                             <img src="images/Twitch.jpg"/>
-                        </li>
-                        <li id="js-work-grid-item-2" className="work-grid-list-item  work-grid-list-item-courant"
-                            // onClick="addOverlay('POCS')"
-                        >
+                            <Carousel.Caption>
+                                <h3 className={'text--medium'}>What Do We Mean by Interaction?</h3>
+                                <p className={'text--paragraph'}>Twitch Channel</p>
+                            </Carousel.Caption>
+                        </Carousel.Item>
+                        <Carousel.Item className="work-grid-list-item">
                             <img src="images/POCS_1.png"/>
-                        </li>
-                        <li id="js-work-grid-item-3" className="work-grid-list-item">
+                            <Carousel.Caption>
+                                <h3 className={'text--medium'}>What Do We Mean by Interaction?</h3>
+                                <p className={'text--paragraph'}>Twitch Channel</p>
+                            </Carousel.Caption>
+                        </Carousel.Item>
+                        <Carousel.Item className="work-grid-list-item">
                             <img src="images/Cytoo.png"/>
-                        </li>
-                        <li id="js-work-grid-item-4" className="work-grid-list-item">
+                            <Carousel.Caption>
+                                <h3 className={'text--medium'}>What Do We Mean by Interaction?</h3>
+                                <p className={'text--paragraph'}>Twitch Channel</p>
+                            </Carousel.Caption>
+                        </Carousel.Item>
+                        <Carousel.Item className="work-grid-list-item">
                             <img src="images/Artelia.jpg"/>
-                        </li>
-                        <li id="js-work-grid-item-5" className="work-grid-list-item">
+                            <Carousel.Caption>
+                                <h3 className={'text--medium'}>What Do We Mean by Interaction?</h3>
+                                <p className={'text--paragraph'}>Twitch Channel</p>
+                            </Carousel.Caption>
+                        </Carousel.Item>
+                        <Carousel.Item className="work-grid-list-item">
                             <img src="images/Twitch.jpg"/>
-                        </li>
-                        <li id="js-work-grid-item-6" className="work-grid-list-item">
-                            <img src="images/Lappart.png"/>
-                        </li>
-                    </ul>
+                            <Carousel.Caption>
+                                <h3 className={'text--medium'}>What Do We Mean by Interaction?</h3>
+                                <p className={'text--paragraph'}>Twitch Channel</p>
+                            </Carousel.Caption>
+                        </Carousel.Item>
+                        <Carousel.Item className="work-grid-list-item">
+                            <img src="images/Lappart1.png"/>
+                            <Carousel.Caption>
+                                <h3 className={'text--medium'}>What Do We Mean by Interaction?</h3>
+                                <p className={'text--paragraph'}>Twitch Channel</p>
+                            </Carousel.Caption>
+                        </Carousel.Item>
+                    </Carousel>
                 </div>
                 <div id="js-work-hover" className="work-hover">
                     <div id="js-work-hover-text" className="work-hover-text">
@@ -76,7 +130,7 @@ function App() {
                     </div>
                 </div>
             </div>
-            <div id="page-2" className="page page--light">
+            <div ref={pageRefs[2]} id="page-2" className="page page--light">
                 <div className="blog-container">
                     <div className="blog-container-text">
                         <span>Blog coming soon...</span>
@@ -86,34 +140,30 @@ function App() {
                     </div>
                 </div>
             </div>
-            <div id="page-3" className="page page--dark">
+            <div ref={pageRefs[3]} id="page-3" className="page page--dark">
+                <img className="about-photo" src="images/profil.jpg"/>
                 <div className="about-container">
-                    <div className="about-text">
-                        <p>
+                    <div className="about-text text--medium">
+                        <div>
                             I'm a french computer science and applied mathematics student based in Copenhagen.
-                        </p>
-                        <p>
+                        </div>
+                        <div>
                             I'm a huge fan of Art though. So I'm trying to link these two fields within my work.
                             Don't hesitate to take a look to my Data Visualization work!
-                        </p>
-                        If you want to contact me, you can send me an email at :
-                        <a href="mailto:rospars.quentin@outlook.com" target="_top">rospars.quentin@outlook.com</a>
-                    </div>
-                    <div className="about-logos">
-                        <div className="about-logos-container">
-                            <a href="https://www.linkedin.com/in/quentin-rospars-a073b6150/" target="_blank"><i
-                                className="fab fa-linkedin-in"></i></a>
-                            <a href="https://www.facebook.com/RosparsQuentin/" target="_blank"><i
-                                className="fab fa-facebook-f"></i></a>
-                            <a href="https://twitter.com/Miionu" target="_blank"><i className="fab fa-twitter"></i></a>
-                            <a href="https://github.com/qrospars" target="_blank"><i
-                                className="fab fa-github-alt"></i></a>
                         </div>
-
+                        <div>If you want to contact me, you can send me an email at :
+                            <a href="mailto:rospars.quentin@outlook.com" target="_top">rospars.quentin@outlook.com</a>
+                        </div>
                     </div>
-                </div>
-                <div className="about-photo-container">
-                    <img className="about-photo" src="images/profil.jpg"/>
+                    <div className="about-logos-container">
+                        <a href="https://www.linkedin.com/in/quentin-rospars-a073b6150/" target="_blank"><i
+                            className="fab fa-linkedin-in"></i></a>
+                        <a href="https://www.facebook.com/RosparsQuentin/" target="_blank"><i
+                            className="fab fa-facebook-f"></i></a>
+                        <a href="https://twitter.com/Miionu" target="_blank"><i className="fab fa-twitter"></i></a>
+                        <a href="https://github.com/qrospars" target="_blank"><i
+                            className="fab fa-github-alt"></i></a>
+                    </div>
                 </div>
             </div>
         </div>
