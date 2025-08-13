a4086d359435d3b349f40c4a7968ad32d3125b46
QuentinRospars <quentin.rospars@valtech.com>
2025-08-13 18:32:52 +0200

    fixing profile description

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
-import React, {useEffect, useRef, useState} from 'react';
-import {Parallax} from 'react-scroll-parallax';
+import React, { useEffect, useRef, useState } from 'react';
+import { Parallax } from 'react-scroll-parallax';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ProjectCarousel from "./components/ProjectCarousel";
@@ -9,8 +9,8 @@ import Artelia from "./pages/Artelia";
 import POCS from "./pages/POCS";
 import Cytoo from "./pages/Cytoo";
 import Discover from "./pages/Discover";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faArrowLeft} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 
 function App() {
     // const { ref2 } = useParallax({ speed: 10 });
@@ -24,7 +24,7 @@ function App() {
 
             const callback = (entries, observer) => {
                 entries.forEach(entry => {
-                    const {target} = entry;
+                    const { target } = entry;
                     if (entry.intersectionRatio >= 0.75) {
                         target.classList.add("is-visible");
                     } else {
@@ -71,7 +71,7 @@ function App() {
 
     const [isOverlayVisible, setIsOverlayVisible] = useState(false);
     const [additionalClass, setAdditionalClass] = useState('');
-    const [currentOverlayContent, setCurrentOverlayContent] = useState(<Artelia/>);
+    const [currentOverlayContent, setCurrentOverlayContent] = useState(<Artelia />);
     const defaultSpeed = 10000;
     const [pauseCarousel, setPauseCarousel] = useState(defaultSpeed);
     const myRef = useRef(null)
@@ -101,16 +101,16 @@ function App() {
     return (
         <div id={'app'} className="App">
             <div className="home text--paragraph font--title textColor--white">
-                <a onClick={() => document.getElementById("page-0").scrollIntoView({behavior: 'smooth'})}>
+                <a onClick={() => document.getElementById("page-0").scrollIntoView({ behavior: 'smooth' })}>
                     <span id={"homeButton"}>Quentin Rospars</span>
                 </a>
             </div>
             <div className="home contact text--paragraph font--title textColor--white">
-                <a onClick={() => document.getElementById("page-3").scrollIntoView({behavior: 'smooth'})}>
+                <a onClick={() => document.getElementById("page-3").scrollIntoView({ behavior: 'smooth' })}>
                     <span>Contact</span>
                 </a>
             </div>
-            <div id={'canvas'}/>
+            <div id={'canvas'} />
             <div className={"scroll-container"}>
                 <div id={"scrollContainer"} className={"scroll-container2"}>
                     <div id="page-0" className="page">
@@ -161,7 +161,7 @@ function App() {
                                 <div w3-include-html="pages/projects_sumUp/POCS.html"></div>
                             </div>
                             <div className="work-hover-return"
-                                // onClick="removeOverlay()"
+                            // onClick="removeOverlay()"
                             >
                                 <i className="fas fa-caret-right"></i>
                             </div>
@@ -174,31 +174,31 @@ function App() {
                             </div>
                             <div className="blog-container-image">
                                 <img className={"img1"}
-                                     src={process.env.PUBLIC_URL + "/images/my_universe/petri photography work.png"}
-                                     alt={"My_Universe_1"}/>
+                                    src={process.env.PUBLIC_URL + "/images/my_universe/petri photography work.png"}
+                                    alt={"My_Universe_1"} />
                                 <img className={"img2"}
-                                     src={process.env.PUBLIC_URL + "/images/my_universe/attraction 1.png"}
-                                     alt={"My_Universe_2"}/>
+                                    src={process.env.PUBLIC_URL + "/images/my_universe/attraction 1.png"}
+                                    alt={"My_Universe_2"} />
                                 <img className={"img3"}
-                                     src={process.env.PUBLIC_URL + "/images/my_universe/Photography  series programatic deformation.png"}
-                                     alt={"My_Universe_3"}/>
+                                    src={process.env.PUBLIC_URL + "/images/my_universe/Photography  series programatic deformation.png"}
+                                    alt={"My_Universe_3"} />
                                 <div className={"button--square text--subtitle"}
-                                     onClick={() => showOverlay(<Discover myRef={myRef}/>, 'discoverOverlay')}>
+                                    onClick={() => showOverlay(<Discover myRef={myRef} />, 'discoverOverlay')}>
                                     Discover
                                 </div>
                             </div>
                         </div>
                     </div>
                     <div id="page-3" className="page page--dark">
-                        <img className="about-photo" src={process.env.PUBLIC_URL + "/images/profil.jpg"}/>
+                        <img className="about-photo" src={process.env.PUBLIC_URL + "/images/profil.jpg"} />
                         <div className="about-container">
                             <div className="about-text text--medium font--paragraph data" data-content>
                                 <div>
                                     I'm a french computer scientist working as a UX optimisation consultant for <a
-                                    href={'https://ecapacity.com/'} target={'_blank'}
-                                    style={{color: 'white'}}>eCapacity</a>_<a
-                                    href={'https://www.valtech.com'} target={'_blank'}
-                                    style={{color: 'white'}}>Valtech</a> in Copenhagen.
+                                        href={'https://ecapacity.com/'} target={'_blank'}
+                                        style={{ color: 'white' }}>eCapacity</a>_<a
+                                            href={'https://www.valtech.com'} target={'_blank'}
+                                            style={{ color: 'white' }}>Valtech</a> in Copenhagen.
                                 </div>
                                 <div>
                                     I always have been passionate about Art & Coding, and I try to link these two
@@ -209,7 +209,7 @@ function App() {
                                     If you think I am someone interesting to talk to, don't hesitate to send me an
                                     email
                                     <a href="mailto:rospars.quentin@outlook.com"
-                                       target="_top" style={{color: 'white'}}> rospars.quentin@outlook.com</a> or
+                                        target="_top" style={{ color: 'white' }}> rospars.quentin@outlook.com</a> or
                                     follow
                                     me on my social media! ❤
                                 </div>
@@ -217,10 +217,8 @@ function App() {
                             <div className="about-logos-container">
                                 <a href="https://www.linkedin.com/in/quentin-rospars-a073b6150/" target="_blank"><i
                                     className="fab fa-linkedin-in"></i></a>
-                                <a href="https://www.facebook.com/RosparsQuentin/" target="_blank"><i
-                                    className="fab fa-facebook-f"></i></a>
-                                <a href="https://twitter.com/Miionu"
-                                   target="_blank"><i className="fab fa-twitter"></i></a>
+                                <a href="https://www.instagram.com/quentin_rsprs/" target="_blank"><i
+                                    className="fab fa-instagram"></i></a>
                                 <a href="https://github.com/qrospars" target="_blank"><i
                                     className="fab fa-github-alt"></i></a>
                             </div>
@@ -228,8 +226,8 @@ function App() {
                     </div>
                     <div id={`carouselOverlay`} className={overlayClasses}>
                         <div className={'backIcon'} onClick={hideOverlay}>
-                            <div style={{position: 'sticky'}}>
-                                <FontAwesomeIcon icon={faArrowLeft}/></div>
+                            <div style={{ position: 'sticky' }}>
+                                <FontAwesomeIcon icon={faArrowLeft} /></div>
                         </div>
                         {currentOverlayContent}
                     </div>
--- src/pages/POCS.js
+++ src/pages/POCS.js
@@ -6,7 +6,7 @@ export default function POCS() {
     return (
         <div className={'work-hover'}>
             <h1 className="work-hover-text-title">
-                Data visualization project<br/>
+                Data visualization project<br />
                 "What do we mean by interaction?"
 
             </h1>
@@ -14,23 +14,23 @@ export default function POCS() {
                 <p className="work-hover-text-content-p">
                     In 2019, I developed a web application allowing the user to explore a dataset of the last 35 years of research papers
                     in the field of Human-Computer Interaction. It took me 6 months to make it.
-                    <br/>
-                        I used technologies such as React and D3 to make it. Making it taught me a lot about User Experience design within
-                        a web app development.
+                    <br />
+                    I used technologies such as React and D3 to make it. Making it taught me a lot about User Experience design within
+                    a web app development.
                 </p>
-                <Zoom><img className="work-hover-text-content-im" src={`${process.env.PUBLIC_URL}/images/POCS_1.png`}/></Zoom>
+                <Zoom><img className="work-hover-text-content-im" src={`${process.env.PUBLIC_URL}/images/POCS_1.png`} /></Zoom>
                 <p className="work-hover-text-content-p">
                     It was text-related data, so I had to find meaningful ways of representing that type of data. But also give powerful
                     exploration tools to the user. For example, the user can analyze the dataset in its whole or at a paper-level.
                     I used Python for data cleaning and Javascript (JSON objects) for making the objects that D3 will use to make the graphs.
                 </p>
-                <Zoom><img className="work-hover-text-content-im" src={`${process.env.PUBLIC_URL}/images/POCS_2.png`}/></Zoom>
+                <Zoom><img className="work-hover-text-content-im" src={`${process.env.PUBLIC_URL}/images/POCS_2.png`} /></Zoom>
                 <p className="work-hover-text-content-p">
                     I also implemented an OAuth connexion system to give the website's access only to a few people since the dataset
                     is confidential.
                 </p>
-                <Zoom><img className="work-hover-text-content-im" src={`${process.env.PUBLIC_URL}/images/POCS_3.png`}/></Zoom>
-                <Zoom><img className="work-hover-text-content-im" src={`${process.env.PUBLIC_URL}/images/POCS_4.png`}/></Zoom>
+                <Zoom><img className="work-hover-text-content-im" src={`${process.env.PUBLIC_URL}/images/POCS_3.png`} /></Zoom>
+                <Zoom><img className="work-hover-text-content-im" src={`${process.env.PUBLIC_URL}/images/POCS_4.png`} /></Zoom>
             </div>
         </div>
     )
